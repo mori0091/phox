15f1d1bf2c8b1a3b31aa1ffb70c82bbe97949c7e
mori0091 <mori-d@qc4.so-net.ne.jp>
2025-11-05 17:29:28 +0900

    refactor: removed useless method `RootModules::get_mut`

--- src/module/rootmodules.rs
+++ src/module/rootmodules.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use super::{Module, RefModule};
+use super::*;
 
 pub struct RootModules {
     map: HashMap<String, RefModule>,
@@ -18,7 +18,7 @@ impl RootModules {
         self.map.get(name).cloned()
     }
 
-    pub fn get_mut(&self, name: &str) -> Option<std::cell::RefMut<'_, Module>> {
-        self.map.get(name).map(|m| m.borrow_mut())
-    }
+    // pub fn get_mut(&self, name: &str) -> Option<std::cell::RefMut<'_, Module>> {
+    //     self.map.get(name).map(|m| m.borrow_mut())
+    // }
 }
