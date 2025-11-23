2aa6e8d40633d4362759f4fe7d091752d4fba18d
mori0091 <mori-d@qc4.so-net.ne.jp>
2025-11-24 06:03:49 +0900

    feat: Paths referencing aliases are now available.
    Example:
    ```
    mod foo {
      let bar = 1;
      use bar as baz;
      use ::prelude::map as list_map;
    };
    
    foo::bar;       // => 1: Int
    foo::baz;       // (equivalent to `foo::bar`)
    foo::list_map;  // (equivalent to `::prelude::map`)
    
    use foo::*;     // (importing variables, functions, types, traits, and also aliases)
    bar;            // (equivalent to `foo::bar`)
    baz;            // (equivalent to `foo::bar`)
    list_map;       // (equivalent to `::prelude::map`)
    ```

--- src/resolve/stmt_use.rs
+++ src/resolve/stmt_use.rs
@@ -24,19 +24,30 @@ pub fn resolve_stmt_use(
                         let other_symbol_env = phox.get_symbol_env(&m);
                         for (p, _sym) in other_symbol_env.clone_map().iter() {
                             let elem = p.head().unwrap(); // PathComponent::Name(name)
-                            let name = elem.to_string();
-                            let alias = &name;
+                            let alias = &elem.to_string();
                             let path = &m.borrow().path().concat(&[elem]);
                             module.borrow_mut().add_alias(alias, path)?;
+                            make_symbol(phox, module, symbol_env, alias)?;
+                        }
+                        for (alias, p) in m.borrow().using.iter() {
+                            let path = &m.borrow().path().concat_path(p);
+                            module.borrow_mut().add_alias(&alias, path)?;
                             make_symbol(phox, module, symbol_env, &alias)?;
                         }
                     }
-                    PathComponent::Name(_name) => {
+                    PathComponent::Name(name) => {
+                        eprintln!(">> use {} as {} / name = {}", path, alias, name);
                         let other_symbol_env = phox.get_symbol_env(&m);
                         if let Some(_sym) = other_symbol_env.get(&rem) {
                             module.borrow_mut().add_alias(&alias, path)?;
                             make_symbol(phox, module, symbol_env, &alias)?;
-                        } else {
+                        }
+                        else if let Some(p) = m.borrow().using.get(&name) {
+                            let path = &m.borrow().path().concat_path(p);
+                            module.borrow_mut().add_alias(&alias, path)?;
+                            make_symbol(phox, module, symbol_env, &alias)?;
+                        }
+                        else {
                             return Err(TypeError::UnknownPath(rem.clone()))
                         }
                     }
